6ed0266a10e45790d46defb32b9d74acc27798c3
tim <dhobleharshwardhan@gmail.com>
2024-03-13 15:18:09 +0530

    Rendering the entites from your DB

--- Frontend/my_vite_app/src/components/LandingPage.jsx
+++ Frontend/my_vite_app/src/components/LandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import images from './data.json';
+// import images from './data.json';
 import '../App.css';
 
-function Images(props) {
+function Images({data:images}) {
 
     return (
         <div className="superhero-div">
@@ -10,7 +10,7 @@ function Images(props) {
                 images && images.map((image, i) => {
                     return (
                         <div key={i} className="superhero">
-                            <img src={image["Images"]} alt={image["Superhero Name"]} />
+                            <img src={image.Images} alt={image["Superhero Name"]} />
                             <h1>{image["Superhero Name"]}</h1>
                             <p>{image["Category"]}</p>
                             <p>{image["First appearance"]}</p>
